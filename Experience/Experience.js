1be687ccec3498f585b52c5419a6c3076a20183c
Joao Pedro Lima <joaopedrolimateixeira@outlook.com>
2023-04-28 18:43:17 -0300

    Experience

---
+++ Experience/Experience.js
@@ -0,0 +1,12 @@
+import * as THREE from 'three';
+
+export default class Experience {
+    static instance
+    constructor(canvas) {
+        if(Experience.instance){
+            return Experience.instance;
+        }
+        Experience.instance = this;
+        this.canvas = canvas;
+    }
+}
---
+++ Experience/Utils/Sizes.js
@@ -0,0 +1,7 @@
+export default class Sizes{
+    constructor(){
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+        
+    }
+}
