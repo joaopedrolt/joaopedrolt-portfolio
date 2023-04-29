46c7e33acbabacac41c11d402c6a84bb186563f5
João Pedro Lima Teixeira <joaopedrolimateixeira@outlook.com>
2023-04-29 18:56:07 -0300

    Singleton

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -1,5 +1,10 @@
 import * as THREE from 'three';
 
+import Sizes from './Utils/Sizes';
+
+import Camera from './Camera';
+import Renderer from './Renderer';
+
 export default class Experience {
     static instance
     constructor(canvas) {
@@ -8,5 +13,9 @@ export default class Experience {
         }
         Experience.instance = this;
         this.canvas = canvas;
+        this.scene = new THREE.Scene();
+        this.sizes = new Sizes();
+        this.camera = new Camera();
+        this.renderer = new Renderer();
     }
 }
---
+++ Experience/Utils/Time.js
@@ -0,0 +1,14 @@
+export default class Time {
+    constructor() {
+        this.start = Date.now();
+        this.current = this.start;
+        this.elapsed = 0;
+        this.delta = 16;
+
+        this.update();
+    }
+
+    update(){
+        
+    }
+}
